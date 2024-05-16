51fe246389f75586f6857b82c5674652da45d771
thulasikan <thulasikanswe@gmail.com>
2024-05-16 22:38:03 +0530

    hello C

---
+++ helloworld.c
@@ -0,0 +1,24 @@
+// this is a comment in C
+//  std-standard io- input/
+#include <stdio.h>
+
+int main()
+{
+    /*
+    \n - newline escape sequence
+    \t - tab escape sequence
+    */
+    printf("hello world\n");
+    printf("I run C\n");
+
+    printf("1\t2\t3\t\n");
+    
+    // creating a grid using tab esc seq
+    printf("\n1\t2\t3\t\n");
+    printf("4\t5\t6\t\n");
+    printf("7\t8\t9\t\n");
+
+    // using quotes
+    printf("\"I'm a C programmer\"\n");
+    return 0;
+}
