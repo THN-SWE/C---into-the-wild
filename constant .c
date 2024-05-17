0a1e09c6f2c8dd61398a69a6f07849dedcb224c3
thulasikan <thulasikanswe@gmail.com>
2024-05-17 12:45:21 +0530

    creating constant variable

---
+++ constant .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    // constant  - a fixed value that cannot be altered by the program while execution
+    // keyword - const
+    //  its a good practice to make const variable name all CAPITAL.
+    const float PI = 3.14159;
+
+    printf("%f",PI)
+
+    return 0;
+}
