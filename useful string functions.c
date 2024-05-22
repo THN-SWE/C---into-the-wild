27b29c4fe60cae11f0b51f81af03a7187fb68916
thulasikan <thulasikanswe@gmail.com>
2024-05-23 00:09:05 +0530

    learned about essential string function in C

---
+++ useful string functions.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char string1[] = "Engineer";
+    char string1[] = "Thulasikan";
+
+    // strlwer(string1);                 //converts into lower case
+    // strup(string2);                   //converts into upper case
+    // strcat(string1, string2);         //appends string 2 to the end of string 1
+    // strncat(string1, string2, 1);     // appends n charecters from string 2 to string 1
+    // strcpy(string1, string2);         //copy string 2 to string 1
+    // strncpy(string1, string2, 2);     //copy n charecters of string 2 to string 1
+
+    // strset(string1, '?');             //set all charecters of the string to given charecter
+    // strnset(string1. 'x', 1);         //set first n charecters of the string to given charecter
+    // strrev(string1);                  //reverses the string
+
+    // int result = strlen(string1);                      // returns string length as int
+    // int result = strcmp(string1, string2);             //string compare all charecters
+    // int result = strncmp(string1, string2, 2);         //string compare n charecters
+    // int result = strcmpi(string1, string1);            // compare all charecters ignore case
+    // int result = strnicmp(string1, string1, 1);        //string compare n charecters (ignore case)
+
+    return 0;
+}
