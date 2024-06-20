696f0422b14c33a83d12bf1aa71e7c004f032cad
thulasikan <thulasikanswe@gmail.com>
2024-06-20 15:27:52 +0530

    2d arrays

---
+++ 2d array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // 2d array where each element is an entire array
+    // useful if you need a matrix, grid or table of data.
+
+    // 2d array with 5 elements max ,each consists of an array of 3 elements
+    int numbers[5][3] = {{1, 2, 3} {4, 5, 6}};
+
+    
+    return 0;
+}
